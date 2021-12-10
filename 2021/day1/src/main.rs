30b6e72c7f5bab5496796f14320aaf4db1301b48
Julien ROLLIN <1086565+jrollin@users.noreply.github.com>
2021-12-10 19:40:41 +0100

    add tests to day1

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,24 +1,29 @@
 use std::fs;
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    part_one(&contents);
-    part_bis(&contents);
+    let count = part_one(&contents);
+    println!("part1: {}", count);
+
+    let count = part_bis(&contents);
+    println!("part2: {}", count);
 }
 
-fn part_one(contents: &String) {
+fn part_one(contents: &str) -> usize {
     let lines = contents
+        .trim()
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| line.trim())
         .map(|line| line.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
-    let iter = lines.windows(2).filter(|pair| pair[0] < pair[1]).count();
-    dbg!(&iter);
-    ();
+    let nb = lines.windows(2).filter(|pair| pair[0] < pair[1]).count();
+    dbg!(&nb);
+    nb
 }
 
-fn part_bis(contents: &String) {
+fn part_bis(contents: &str) -> usize {
     let lines = contents
+        .trim()
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| line.trim())
@@ -29,7 +34,34 @@ fn part_bis(contents: &String) {
         .windows(3)
         .map(|t| t.iter().sum())
         .collect::<Vec<i32>>();
-    let iter = ter.windows(2).filter(|pair| pair[0] < pair[1]).count();
-    dbg!(&iter);
-    ();
+    let nb = ter.windows(2).filter(|pair| pair[0] < pair[1]).count();
+    nb
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    const INPUT: &str = r"
+        199 
+        200 
+        208 
+        210 
+        200 
+        207 
+        240 
+        269 
+        260 
+        263 
+    ";
+
+    #[test]
+    fn one() {
+        assert_eq!(part_one(INPUT), 7)
+    }
+
+    #[test]
+    fn two() {
+        assert_eq!(part_bis(INPUT), 5)
+    }
 }
